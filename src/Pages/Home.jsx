b385e8c01267dfaa2169cf63452ff7d0c9dcbe0a
mohitgarg05 <mgarg6858@gmail.com>
2025-05-10 19:32:57 +0530

    process section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import QualityInformation from '../components/V1/QualityInformation.jsx'
-import Procurenet from '../components/V1/Procurenet.jsx'
-import ImpactStory from '../components/V1/ImpactStory.jsx'
-import OurWork from '../components/V1/OurWork.jsx'
-import DeliveryProcess from '../components/V1/DeliveryProcess.jsx'
+import QualityInformation from '../components/V1/QualityInformation'
+import Procurenet from '../components/V1/Procurenet'
+import ImpactStory from '../components/V1/ImpactStory'
+import OurWork from '../components/V1/OurWork'
+import DeliveryProcess from '../components/V1/DeliveryProcess'
 
 
 
