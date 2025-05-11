4a8b03e67d3d817dbf5883a21dba4d49e281b4d7
mohitgarg05 <mgarg6858@gmail.com>
2025-05-11 16:31:50 +0530

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,30 @@
-import QualityInformation from '../Components/V1/QualityInformation'
-import Procurenet from '../Components/V1/Procurenet'
-import ImpactStory from '../Components/V1/ImpactStory'
-import OurClient from '../Components/V1/OurClient'
-import OurWork from '../components/V1/OurWork'
-import DeliveryProcess from '../components/V1/DeliveryProcess'
-import AgileApproch from '../components/V1/AgileApproch'
-import OurImpact from '../Components/V1/OurImpact'
+import QualityInformation from "../Components/V1/QualityInformation";
+import Procurenet from "../Components/V1/Procurenet";
+import ImpactStory from "../Components/V1/ImpactStory";
+import OurClient from "../Components/V1/OurClient";
+import OurWork from "../components/V1/OurWork";
+import DeliveryProcess from "../components/V1/DeliveryProcess";
+import AgileApproch from "../components/V1/AgileApproch";
+import OurImpact from "../Components/V1/OurImpact";
 
 function Home() {
-    return (
-        <>
-            <div className='flex flex-col items-center justify-center h-full'>
-                <h1 className='text-red-700'>Home Page Coming Soon</h1>
-                <QualityInformation />
-                <DeliveryProcess />
-                <Procurenet />
-                <ImpactStory />
-                <OurWork />
-                <OurClient />
-                <OurImpact />
-                {/* <Link to="/contact">Contact</Link> */}
-            </div>
-            <AgileApproch />
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-red-700">Home Page Coming Soon</h1>
+        <QualityInformation />
+        <DeliveryProcess />
+        <Procurenet />
+        <ImpactStory />
+        <OurWork />
+      </div>
+      <AgileApproch />
+      <div className="flex flex-col items-center justify-center h-full">
+        <OurClient />
+        <OurImpact />
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
